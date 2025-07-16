70dd571000ff5890535c4dc8e9d70e31d035fd03
Gabriel Novais <silvanovaesgabriel@gmail.com>
2025-07-16 14:30:25 -0300

    Fixing routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,16 @@
 import App from "./App";
 import "./index.css";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { HashRouter as Router, Routes, Route } from "react-router";
 import EditTransaction from "./pages/EditTransaction";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/edittransaction",
-    element: <EditTransaction />,
-  },
-]);
 
 const root = document.getElementById("root");
 
-ReactDOM.createRoot(root).render(<RouterProvider router={router} />);
+ReactDOM.createRoot(root).render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/edittransaction" element={<EditTransaction />} />
+    </Routes>
+  </Router>
+);
