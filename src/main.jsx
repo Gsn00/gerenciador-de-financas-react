303f7d1f8ebb938c1a6c18977bda8e743897fb66
Gabriel Novais <silvanovaesgabriel@gmail.com>
2025-07-16 14:33:45 -0300

    removing route configs

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
 import App from "./App";
 import "./index.css";
 import ReactDOM from "react-dom/client";
-import { HashRouter as Router, Routes, Route } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import EditTransaction from "./pages/EditTransaction";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/edittransaction",
+    element: <EditTransaction />,
+  },
+]);
 
 const root = document.getElementById("root");
 
-ReactDOM.createRoot(root).render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/edittransaction" element={<EditTransaction />} />
-    </Routes>
-  </Router>
-);
+ReactDOM.createRoot(root).render(<RouterProvider router={router} />);
