19b0a32105ef1d41e965e3a2b1a61252ea74585d
Gabriel Novais <silvanovaesgabriel@gmail.com>
2025-07-17 21:03:16 -0300

    Input width

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,7 +2,7 @@ function Input(props) {
   return (
     <input
       {...props}
-      className={`border border-gray-300 outline-gray-800 rounded-md py-0.5 px-3 ${props.className}`}
+      className={`w-full border border-gray-300 outline-gray-800 rounded-md py-0.5 px-3 ${props.className}`}
     />
   );
 }
